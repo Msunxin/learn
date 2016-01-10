f9620575b98d96c75a4bdc0f60361a0c2a01817b
sunxin <352917208@qq.com>
2016-01-10 15:25:20 +0800

    login

--- Apps/Home/Controller/BaseController.class.php
+++ Apps/Home/Controller/BaseController.class.php
@@ -18,11 +18,49 @@ class BaseController extends Controller {
     public $skinpath;
     public function __construct(){
         parent::__construct();
+        session_start();
         $this->_init();
         $this->skinpath = '/Apps/Home/View/';
     }
     protected function _init(){
+        $this->user = $_SESSION['user'];
         $this->assign('skinpath',$this->skinpath);
     }
+    
+    public STATIC function int($name,$default=null){
+        $name = $_REQUEST[$name];
+        if(!isset($name)){
+            return $default;
+        }
+        if(is_int($name)){
+            return $name;
+        }else{
+            return (int)$name;
+        }
+     }
+     
+     public static function string($name,$default=null){
+         $name = $_REQUEST[$name];
+         if(!isset($name)){
+            return $default;
+        }
+         if(is_string($name)){
+             return $name;
+         }else{
+             return (string)$name;
+         }
+     } 
+     
+     public static function arrays($name,$default=null){ 
+         $name = $_REQUEST[$name];    
+         if(!isset($name)){
+            return $default;
+        }
+        if(is_array($name)){
+            return $name;
+        }else{
+            return (array)$name;
+        }
+     } 
 }
 
--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -3,12 +3,24 @@ namespace Home\Controller;
 class IndexController extends BaseController {
     public function __construct() {
         parent::__construct();       
+        $this->id = 'sun';
+        $this->psw = '111';
     }
-    public function index(){       
+    public function index(){    
         $this->assign('skinpath',$this->skinpath);
         $this->display('skin:index');
     }
     public function login(){
-        echo 11;
+        $a = BaseController::int('id');
+        $b = BaseController::int('psw');
+        if($a == $this->id && $b == $this->psw){
+            $res = array('code'=>200,'msg'=>'登录成功','id'=>$a,'time'=>time(),'ip'=>$_SERVER['REMOTE_ADDR']);
+            $_SESSION['user'] = array('code'=>200,'msg'=>'登录成功','id'=>$a,'time'=>time(),'ip'=>$_SERVER['remote_addr']);
+            echo json_encode($res);
+        }else{
+            $res = array('code'=>500,'msg'=>'登录失败');
+            echo json_encode($res);
+        }
+        die;
     }
 }
